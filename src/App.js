2b604794855cf6ec97fbd7c06f5db18922e94400
Daniel N Somoano <dnsomoano@gmail.com>
2018-10-26 23:09:04 -0400

    Fixed table alignment and added media queries for phones and tablets.

--- src/App.js
+++ src/App.js
@@ -9,20 +9,25 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">List of Crypto Currencies</h1>
         </header>
-        <table>
-          <tbody>
-            <tr>
-              <th>Rank</th>
-              <th className="currencyName">Icon</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th className="hourlyChange">% Change (1hr)</th>
-              <th className="dailyChange">% Change (24h)</th>
-              <th>% Change (7d)</th>
-            </tr>
-            <CoinList />
-          </tbody>
-        </table>
+        <section className="body">
+          <table className="coin-table">
+            <tbody>
+              <tr>
+                <th className="coin-rank-header">Rank</th>
+                <th className="currencyName">Icon</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th className="hourlyChange">% Change (1hr)</th>
+                <th className="coin-change-24h">% Change (24h)</th>
+                <th className="coin-change-7d-header">% Change (7d)</th>
+              </tr>
+              <CoinList />
+            </tbody>
+          </table>
+        </section>
+        <a href="https://github.com/dnsomoano">
+          <footer id="dev-footer">® Created by Daniel N Somoano</footer>
+        </a>
       </div>
     );
   }
--- src/Coin.js
+++ src/Coin.js
@@ -3,21 +3,22 @@ import React, { Component } from "react";
 class Coin extends Component {
   render() {
     return (
-        <tr>
-          <td class="coinId">{this.props.rank}</td>
-          <td class="coinIcon">
-            <img
-              src={`https://s2.coinmarketcap.com/static/img/coins/16x16/${
-                this.props.rank
-              }.png`}
-            />
-          </td>
-          <td class="coinName">{this.props.name}</td>
-          <td class="coinPrice">{this.props.price}</td>
-          <td>{this.props.change1h}%</td>
-          <td>{this.props.change24h}%</td>
-          <td>{this.props.change7d}%</td>
-        </tr>
+      <tr>
+        <td className="coin-rank">{this.props.rank}</td>
+        <td className="coinIcon">
+          <img
+            src={`https://s2.coinmarketcap.com/static/img/coins/16x16/${
+              this.props.rank
+            }.png`}
+            alt={this.props.name}
+          />
+        </td>
+        <td className="coinName">{this.props.name}</td>
+        <td className="coinPrice">{this.props.price}</td>
+        <td>{this.props.change1h}%</td>
+        <td className="coin-change-24h">{this.props.change24h}%</td>
+        <td className="coin-change-7d">{this.props.change7d}%</td>
+      </tr>
     );
   }
 }
